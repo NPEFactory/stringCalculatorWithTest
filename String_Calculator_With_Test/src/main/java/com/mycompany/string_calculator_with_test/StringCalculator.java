464031d3489bbae92ebb314dc779a670b1c9145c
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-02-26 16:09:33 +0100

    Test works with the exeption try .....

--- String_Calculator_With_Test/src/main/java/com/mycompany/string_calculator_with_test/StringCalculator.java
+++ String_Calculator_With_Test/src/main/java/com/mycompany/string_calculator_with_test/StringCalculator.java
@@ -5,23 +5,37 @@
  */
 package com.mycompany.string_calculator_with_test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author boss
  */
 public class StringCalculator {
-    
-    public int add(String str){
-        if (str.isEmpty()){
+
+    public int add(String str) throws NegativesNotAllowed {
+        if (str.isEmpty()) {
             return 0;
-        }        
-        Integer sum =0;
+        }
+
+        String negatives = "";
+        Pattern p = Pattern.compile("-\\d+");
+        Matcher m = p.matcher(str);
+        while (m.find()) {
+            negatives += m.group(0) + ",";
+
+        }
+        if (!negatives.isEmpty()) {            
+            throw new NegativesNotAllowed(negatives.substring(0, negatives.lastIndexOf(',')));
+        }
+        Integer sum = 0;
         String[] numbers = str.split("[^-0123456789]");
         for (String number : numbers) {
             sum += Integer.valueOf(number);
         }
-        
+
         return sum;
     }
-    
+
 }
--- String_Calculator_With_Test/src/test/java/com/mycompany/string_calculator_with_test/StringCalculatorTest.java
+++ String_Calculator_With_Test/src/test/java/com/mycompany/string_calculator_with_test/StringCalculatorTest.java
@@ -5,37 +5,41 @@
  */
 package com.mycompany.string_calculator_with_test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
  * @author boss
  */
 public class StringCalculatorTest {
-    
+
     private StringCalculator calc;
-    
+
     public StringCalculatorTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        calc= new StringCalculator();
+        calc = new StringCalculator();
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -45,37 +49,75 @@ public class StringCalculatorTest {
      */
     @Test
     public void testAdd() {
-          
+
     }
-    
+
     @Test
-    public void executeShouldReturnZeroIfEmptyString(){
-        assertEquals(0, calc.add(""));
+    public void executeShouldReturnZeroIfEmptyString() {
+        try {
+            assertEquals(0, calc.add(""));
+        } catch (NegativesNotAllowed ex) {
+            Logger.getLogger(StringCalculatorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
-    public void executeShouldReturnTheGivenNumber(){
-        assertEquals(1, calc.add("1"));
-        assertEquals(2, calc.add("2"));
+    public void executeShouldReturnTheGivenNumber() {
+        try {
+            assertEquals(1, calc.add("1"));
+            assertEquals(2, calc.add("2"));
+        } catch (NegativesNotAllowed ex) {
+            Logger.getLogger(StringCalculatorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
-    public void executeShouldReturnSumOfTheGivenTwoNumbers(){
-        assertEquals(3, calc.add("1,2"));
-        assertEquals(14, calc.add("10,4"));
+    public void executeShouldReturnSumOfTheGivenTwoNumbers() {
+        try {
+            assertEquals(3, calc.add("1,2"));
+            assertEquals(14, calc.add("10,4"));
+        } catch (NegativesNotAllowed ex) {
+            Logger.getLogger(StringCalculatorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
-    public void executeShouldReturnSumOfTheGivenNumbers(){
-        assertEquals(10, calc.add("1,3,1,5"));
-        assertEquals(35, calc.add("10,4,5,12,4"));
+    public void executeShouldReturnSumOfTheGivenNumbers() {
+        try {
+            assertEquals(10, calc.add("1,3,1,5"));
+            assertEquals(35, calc.add("10,4,5,12,4"));
+        } catch (NegativesNotAllowed ex) {
+            Logger.getLogger(StringCalculatorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
-    public void executeShouldReturnSumOfTheGivenNumbersWithSpecialDelimitters(){
-        assertEquals(3, calc.add("1;2"));
-        assertEquals(8, calc.add("3\n5"));
-        assertEquals(6, calc.add("4\t2"));
-        assertEquals(11, calc.add("6|5"));
+    public void executeShouldReturnSumOfTheGivenNumbersWithSpecialDelimitters() {
+        try {
+            assertEquals(3, calc.add("1;2"));
+            assertEquals(8, calc.add("3\n5"));
+            assertEquals(6, calc.add("4\t2"));
+            assertEquals(11, calc.add("6|5"));
+        } catch (NegativesNotAllowed ex) {
+            Logger.getLogger(StringCalculatorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenNegativesInTheString() {
+        try {
+//            exception.expect(NegativesNotAllowed.class);
+//            exception.expectMessage("-3,-5");
+            calc.add("1,-3,2,-5");
+            fail("failed to drop the exeption...");
+        } catch (Exception ex) {
+            System.out.println("ex: " + ex.getClass());
+            System.out.println("msg: " + ex.getMessage());
+            assertTrue(ex.getMessage().equals("-3,-5"));
+        }
+    }
+
 }
