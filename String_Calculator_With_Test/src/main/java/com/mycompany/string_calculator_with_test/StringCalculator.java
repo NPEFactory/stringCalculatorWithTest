b60c0b4387df3abd3f788b27ad30fe3a6c99b824
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-02-26 09:48:07 +0100

    Class complete :)

---
+++ String_Calculator_With_Test/src/main/java/com/mycompany/string_calculator_with_test/StringCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.string_calculator_with_test;
+
+/**
+ *
+ * @author boss
+ */
+public class StringCalculator {
+    
+    
+    
+}
